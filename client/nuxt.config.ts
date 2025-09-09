c8077596baba26a5ed2f913e3d86bc0a9cfb79a5
Md. Shahrukh Khan <shahrukhkhan3418198@gmail.com>
2025-09-09 20:27:36 +0600

    fixed conflict

--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -24,5 +24,6 @@ export default defineNuxtConfig({
   },
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', ]
+  modules: ['@nuxtjs/tailwindcss'],
+  css: ['~/assets/scss/main.scss']
 });
